21bf3191b844c1266a5265ae5a43d23468999ef4
inhye6-6 <haine4546@gmail.com>
2021-12-30 21:46:05 +0900

    [211230] feedback 받은거 근데 시간 변화 없음 ㅜㅜ 어찌줄이지

--- DFS_BFS/baekjoon_1012_bfs.py
+++ DFS_BFS/baekjoon_1012_bfs.py
@@ -8,6 +8,7 @@ def bfs(graph,n,m,i,j):
     queue=deque()
     queue.append((i,j))
 
+    # 상하좌우 
     dx=[-1,1,0,0]
     dy=[0,0,-1,1]
 
@@ -22,7 +23,7 @@ def bfs(graph,n,m,i,j):
             if nx < 0 or nx >= n or ny < 0 or ny >= m :
                 continue
 
-            if graph[nx][ny] == 1:
+            if graph[nx][ny] == True:
                 graph[nx][ny] = 0 # 노드 방문 처리
                 queue.append((nx,ny))
 
@@ -32,17 +33,17 @@ def bfs_solution():
     for _ in range(T):
         m, n, k = map(int,input().split()) # n*m , k = 배추 개수
 
-        # 그래프 만들기
+        # 그래프 만들기(bool로 처리)
         graph = [ [0 for _ in range(m)] for _ in range(n)] 
         for _ in range(k): 
             x, y = map(int,input().split())
-            graph[y][x] = 1
+            graph[y][x] = True
 
         # 지렁이 배치
         result=0
         for y in range(n):
             for x in range(m):
-                if graph[y][x] == 1:
+                if graph[y][x] == True:
                     graph[y][x] = 0 # 노드 방문 처리
                     bfs(graph,n,m,y,x)
                     result += 1
