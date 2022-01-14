b9fd0e7df6615e04e40fbadc0b07e3e82327cf34
inhye6-6 <haine4546@gmail.com>
2022-01-14 22:47:58 +0900

    [220114]

--- dp/baekjoon_12865.py
+++ dp/baekjoon_12865.py
@@ -3,4 +3,15 @@ input = sys.stdin.readline
 
 n,k = map(int,input().split())
 items = [tuple(map(int,input().split())) for _ in range(n)]
+d = [[0]*
+items.sort(key= lambda x : (x[0],-x[1]))
 
+w = 0
+v = 0
+max_v=0
+
+
+
+for i in range(n-1):
+    w = items[i][0]
+    v = items[i][1]
