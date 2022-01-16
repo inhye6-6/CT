0c34d7012169ecac64a943dd68484aa19f62852d
inhye6-6 <haine4546@gmail.com>
2022-01-16 14:05:54 +0900

    [220116] baekjoon_12865(7608ms 너무 느림)

--- dp/baekjoon_12865.py
+++ dp/baekjoon_12865.py
@@ -1,17 +1,25 @@
+
+# 28104KB, 7608ms
 import sys
 input = sys.stdin.readline
 
 n,k = map(int,input().split())
-items = [tuple(map(int,input().split())) for _ in range(n)]
-d = [[0]*
-items.sort(key= lambda x : (x[0],-x[1]))
+wt = []
+vt=[]
+for _ in range(n):
+    w,v = map(int,input().split())
+    wt.append(w)
+    vt.append(v)
 
-w = 0
-v = 0
-max_v=0
+d = [[0]*(k+1) for _ in range(n+1)]
 
 
 
-for i in range(n-1):
-    w = items[i][0]
-    v = items[i][1]
+for i in range(1,n+1):
+    for j in range(1,k+1) :
+        if wt[i-1] <= j :
+            d[i][j]= max(vt[i-1]+d[i-1][j-wt[i-1]],d[i-1][j])
+        else :
+            d[i][j] = d[i-1][j]
+
+print(d[n][k])
