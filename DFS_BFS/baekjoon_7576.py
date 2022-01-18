8a1d09f26ac2c37eb7bbf91f1ab5733469b95b0d
inhye6-6 <haine4546@gmail.com>
2022-01-19 02:18:09 +0900

    [220118] baekjoon_7576(105868KB,2580ms)

--- DFS_BFS/baekjoon_7576.py
+++ DFS_BFS/baekjoon_7576.py
@@ -1,4 +1,5 @@
 # 토마토
+# 105868KB,	2580ms
 
 import sys 
 from collections import deque
@@ -6,41 +7,47 @@ input = sys.stdin.readline
 
 m, n = map(int,input().split())
 storage=[]
+visited = [[0]*m for _ in range(n)] 
 queue = deque()
-count = 0
+count = 0   # 안익은 토마토 수 count
 
 for i in range(n) : 
     row=list(map(int,input().split()))
     storage.append(row)
 
-    # 익은 토마토 queue에 담기
     for j, tomato in enumerate(row):
+        # 익은 토마토 좌표 queue에 담기
         if tomato == 1 :
             queue.append((i,j))
             continue
+        # 안익은 토마토수 count
         if tomato == 0 :
             count += 1
-
-visited = [[0]*m for _ in range(n)]
 
 
 def bfs():
     global count
-    day = 0
+    day = 0     # 지난 시간 count 
+    
+    # 상하좌우
     dx = [1,-1,0,0]
     dy = [0,0,-1,1]
 
     while queue :
         x,y = queue.popleft()
+        visited[x][y]= 1 
 
-        visited[x][y]= 1
+        #현재 위치에서 상하좌우 확인
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
+            # 창고를 벗어난 경우 무시
             if nx<0 or nx >= n or ny <0 or ny>= m :
                 continue
 
+            # 해당 위치에 안익은 토마토가 있고 
+            # 처음 방문하는 경우에만 시간 기록
             if visited[nx][ny] != 1 and storage[nx][ny] == 0 :
                 queue.append((nx,ny))
                 visited[nx][ny] = 1
@@ -49,11 +56,14 @@ def bfs():
                 count -= 1
     return day
 
+# 저장될 때부터 안익은 토마토가 없는 경우
 if count == 0:
     print('0')
+
 else :
     day = bfs()
+    # bfs를 수행한 뒤에도 안익은 토마토가 존재할 경우
     if count > 0 : 
         print('-1')
-                
+    # 토마토가 다 익을 때까지 최소 날짜       
     else : print(day)
