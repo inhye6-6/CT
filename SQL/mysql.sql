604a4fa74ed57e11f2f6735ebf6be2bab8956731
inhye6-6 <haine4546@gmail.com>
2022-01-29 23:31:14 +0900

    [220129] case when

--- SQL/mysql.sql
+++ SQL/mysql.sql
@@ -184,6 +184,14 @@ where a.cart_id= b.cart_id and a.name like 'Milk' and b.name like 'Yogurt'
 
 
 --
+-- 코드를 입력하세요
 SELECT animal_id, name, sex_upon_intake
 from animal_ins
-where name in ('Lucy','Ella','Pickle','Sabirna','Mitty')
+where name in ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+order by 1
+
+-- case when !
+select animal_id , name, 
+    case when sex_upon_intake like 'Neutered%' or sex_upon_intake like 'Spayed%' then 'O' 
+    else 'X' end as 중성화
+from animal_ins
