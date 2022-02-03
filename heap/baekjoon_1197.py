51703240352d6aec8c1f19ecb67da81f23004191
inhye6-6 <haine4546@gmail.com>
2022-02-04 01:21:01 +0900

    [220204] ver3 (for문 도는거 하나만 줄였는데 차이가 꽤 큼!) (61832KB, 704ms)

--- heap/baekjoon_1197.py
+++ heap/baekjoon_1197.py
@@ -3,6 +3,8 @@
 ## 2. 해당 정점에서 갈수 있는 간선을 minheap에 넣는다.
 ## 3. 최소값을 뽑아 해당 정점을 방문안했다면 선택
 
+# ver 2 
+# 61988KB, 4744ms
 import heapq
 
 v,e = map(int,input().split())
@@ -32,3 +34,36 @@ while heap :
             heapq.heappush(heap,i)
 
 print(answer)
+
+#ver 3
+#61832KB, 704ms
+
+import sys
+import heapq
+input = sys.stdin.readline
+
+v,e = map(int,input().split())
+graph = [[] for _ in range(v)]
+visited = [0]*(v)
+
+for _ in range(e) :
+    a, b ,c = map(int,input().split())
+    graph[a-1].append([c,(b-1)])
+    graph[b-1].append([c,(a-1)])
+
+min_weight=0
+n=0 #간선 수
+heap=[[0,0]]    
+
+while heap :
+    if n == v :
+        break
+    w, s =heapq.heappop(heap)
+    if visited[s] == 0:
+        visited[s] = 1 
+        min_weight+=w
+        n+=1
+        for i in graph[s]:
+            heapq.heappush(heap,i)
+print(answer)
+    
