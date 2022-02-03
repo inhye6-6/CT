a5786a540de715c96f4b93cc52e288d8459bf9a6
inhye6-6 <haine4546@gmail.com>
2022-02-03 19:24:32 +0900

    [220202] 프로그래머스

---
+++ search/오픈채팅방.py
@@ -0,0 +1,20 @@
+
+def solution(record):
+    users={}
+    actions = {"Enter":"님이 들어왔습니다.","Leave":"님이 나갔습니다."}
+    records = []
+    for alarm in record :
+        if alarm.find('Leave') == -1:
+            action, user, nickname =alarm.split()
+            users[user]=nickname
+        else : 
+            action , user = alarm.split()
+        
+        records.append((action,user))
+            
+    answer = []       
+    for act, uid in records :
+        if act != 'Change':
+            answer.append(users[uid]+ actions[act])
+    
+    return answer
