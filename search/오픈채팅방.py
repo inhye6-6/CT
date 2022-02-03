dfdd17977df97ac58540d3156a0170dc5cfba0d7
inhye6-6 <haine4546@gmail.com>
2022-02-03 19:28:07 +0900

    [220203] string 처리 다른 ver

--- search/오픈채팅방.py
+++ search/오픈채팅방.py
@@ -18,3 +18,20 @@ def solution(record):
             answer.append(users[uid]+ actions[act])
     
     return answer
+
+
+def solution(record):
+    users={}
+    actions = {"Enter":"님이 들어왔습니다.","Leave":"님이 나갔습니다."}
+    records = []
+    for alarm in record :
+        if alarm.find('Leave') == -1:
+            action, user, nickname =alarm.split()
+            users[user]=nickname
+            
+    answer = []       
+    for alarm in record :
+        if alarm.split()[0] != 'Change':
+            answer.append(users[alarm.split()[1]]+ actions[alarm.split()[0]])
+    
+    return answer
