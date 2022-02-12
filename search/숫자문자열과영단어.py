5381f4ec25750eee75ce31b0bffd7d90afcbf195
inhye6-6 <haine4546@gmail.com>
2022-02-12 16:25:51 +0900

    [220212] 쉬운ver

--- search/숫자문자열과영단어.py
+++ search/숫자문자열과영단어.py
@@ -17,3 +17,11 @@ def solution(s):
             
     answer = int(answer)
     return answer
+
+    def solution(s):
+        dic = {'zero':'0','one':'1','two':"2",'three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
+
+        for k,v in dic.items():
+            s=s.replace(k,v)
+        s=int(s)
+        return s
