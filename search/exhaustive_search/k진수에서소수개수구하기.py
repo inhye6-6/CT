529b84543007443844ea92cfb30f4e4fbb6b9f02
inhye6-6 <haine4546@gmail.com>
2022-02-12 18:18:02 +0900

    [220212] TLE

---
+++ search/exhaustive_search/k진수에서소수개수구하기.py
@@ -0,0 +1,26 @@
+def is_prime_num(num):
+    num=int(num)
+    if num == 1:
+        return False
+    for i in range(2,(num//2)+1):
+        if num%i == 0 :
+            return False
+    return True
+
+def solution(n, k):
+    tmp = ''
+    while n:
+        tmp += str(n%k)
+        n = n//k
+    n=tmp[::-1]
+    nums = n.split('0')
+    count = 0
+
+
+    for num in nums:
+        if num == '' :
+            continue
+        if is_prime_num(num) :
+            count +=1
+    
+    return count
