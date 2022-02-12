a1d59245247d574e3d3558050627b10790f7d694
inhye6-6 <haine4546@gmail.com>
2022-02-12 18:26:26 +0900

    [220212] 제곱근이다.. 바보야...

--- search/exhaustive_search/k진수에서소수개수구하기.py
+++ search/exhaustive_search/k진수에서소수개수구하기.py
@@ -1,8 +1,9 @@
+import math
 def is_prime_num(num):
     num=int(num)
     if num == 1:
         return False
-    for i in range(2,(num//2)+1):
+    for i in range(2,int(math.sqrt(num))+1):
         if num%i == 0 :
             return False
     return True
