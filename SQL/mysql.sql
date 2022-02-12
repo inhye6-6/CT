f34679b817e28407fb8e4248b7f220fb7e14686a
inhye6-6 <haine4546@gmail.com>
2022-02-13 02:46:36 +0900

    [220213] 문자열 처리 (left,right)

--- SQL/mysql.sql
+++ SQL/mysql.sql
@@ -281,5 +281,10 @@ limit 1)
 
 select distinct city
 from station
-where city like "a%" or  city like "e%" or  city like "i%" or  city like "o%" or  city like "u%" a
+where city like "a%" or  city like "e%" or  city like "i%" or  city like "o%" or  city like "u%" 
+
+
+select distinct city
+from station
+where left(city,1) in ("a","e","i","o","u") and right(city,1) in ("a","e","i","o","u") 
 
