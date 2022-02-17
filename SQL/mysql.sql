1ecede15a1bc600b6613641d7fea99ff8a61d47e
inhye6-6 <haine4546@gmail.com>
2022-02-18 01:44:24 +0900

    [220217] join사용

--- SQL/mysql.sql
+++ SQL/mysql.sql
@@ -394,3 +394,15 @@ case when P is null then "Root"
 when a.N in (select distinct P from BST) then "Inner" else "Leaf" end
 from BST a 
 order by a.N
+
+--join
+select c.company_code, c.founder,
+       count(distinct(l.lead_manager_code)), count(distinct(s.senior_manager_code)),
+       count(distinct(m.manager_code)), count(distinct(e.employee_code))
+from company c
+join lead_manager l on (c.company_code = l.company_code)
+join senior_manager s on (l.lead_manager_code = s.lead_manager_code)
+join manager m on (s.senior_manager_code = m.senior_manager_code)
+join employee e on (m.manager_code = e.manager_code)
+group by c.company_code, c.founder
+order by c.company_code
